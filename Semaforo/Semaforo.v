7b70585c6c20ac9737cacf85259ab808a7c6837a
André Paiva <andre.pcr@hotmail.com>
2021-11-29 15:52:04 -0300

    Semáforo Combinacional v1

--- Semaforo/Semaforo.v
+++ Semaforo/Semaforo.v
@@ -1,5 +1,11 @@
-module Semaforo (a, b, c, d, n_s, l_o);
-input a, b, c, d;
+module Semaforo (a, b, c, d, clk, n_s, l_o);
+input a, b, c, d, clk;
 output reg n_s, l_o;
+
+always @ (negedge clk)
+	begin
+		n_s <= (a || b) && c && d;
+		l_o <= !((a || b) && c && d);
+	end
 
 endmodule
